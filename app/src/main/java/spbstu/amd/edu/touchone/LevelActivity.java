fe0daf0fda5ed294043c57e2a4aa4b76c7ff6390
ukirain <uki.rain@gmail.com>
2018-09-25 13:59:29 +0300

    Canvas
    Drawing Canvas

--- app/src/main/java/spbstu/amd/edu/touchone/LevelActivity.java
+++ app/src/main/java/spbstu/amd/edu/touchone/LevelActivity.java
@@ -1,25 +1,45 @@
 package spbstu.amd.edu.touchone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class LevelActivity extends AppCompatActivity {
 
     ActionBar actionBar;
+    ImageButton ibOne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
 
+        ibOne = findViewById(R.id.ibOne);
+
         actionBar = getSupportActionBar();
         actionBar.setHomeButtonEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        View.OnClickListener oclIbOne = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LevelActivity.this, "Clicked level button", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(LevelActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        ibOne.setOnClickListener(oclIbOne);
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
